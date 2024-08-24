c8896036f4ca0e30b0ddd515f5a27030c8c3fc5e
luna-rm <luna4.marchi@hotmail.com>
2024-08-24 11:45:48 -0300

    a

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import P1 from './imgs/P1.png'
+import P2 from './imgs/P2.png'
+import P3 from './imgs/P3.png'
+import P4 from './imgs/P4.png'
+import qr from './imgs/qr.jpg'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className="AAAAA">
+                <img src={P1} alt='P1' className='imgs'/>
+                <img src={P2} alt='P2' className='imgs'/>
+                <img src={P3} alt='P3' className='imgs'/>
+                <img src={P4} alt='P4' className='imgs'/>
+            </div>
+            {/* <img src={qr} alt='qr' className='qr'/> */}
+        </div>
+    );
 }
 
 export default App;
